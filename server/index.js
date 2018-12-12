56d3d8b761bdfde856d1b1580780ca3739875ec0
Onex101 <xeno@zarobi.co.za>
2018-12-12 12:58:56 +0200

    Changed user model naming convention

--- server/index.js
+++ server/index.js
@@ -15,19 +15,19 @@ var session = require('express-session')
 
 
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(session({   secret: 'matcha',
-                    saveUninitialized: true,
-                    resave: true}))
+// app.use(cookieParser());
+// app.use(session({   secret: 'matcha',
+//                     saveUninitialized: true,
+//                     resave: true}))
 app.use(cors());
 
 var sess;
 app.get('/', (req, res) => {
-    sess = req.session;
-    sess.id;
-    sess.email = 'xeno@zarobi.co.za';
-    sess.user_name = 'hi';
-    res.send('go to /users' + JSON.stringify(sess));
+    // sess = req.session;
+    // sess.id;
+    // sess.email = 'xeno@zarobi.co.za';
+    // sess.user_name = 'hi';
+    res.send('go to /users');
 });
 
 io.on('connection', function (socket) {
