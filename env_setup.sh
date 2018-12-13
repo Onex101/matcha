b6b83626080b538523cd941e7042a2c844c6d7df
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-12-13 18:43:40 +0200

    env and npm instalations split

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,3 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)";
 brew install node;
 brew install mysql;
-npm install nodemailer --save;
-npm install bcrypt --save;
