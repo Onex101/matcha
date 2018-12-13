fca0826fd39e77f1541fdaa7a850f986c3add604
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-12-13 15:40:33 +0200

    db_update

--- server/databaseInit.js
+++ server/databaseInit.js
@@ -177,10 +177,10 @@ connection.connect(function (err) {
 
 	connection.query('ALTER TABLE `users`\
 	ADD COLUMN `bio` VARCHAR(255) AFTER password,\
-	ADD COLUMN `fame` int(9) unsigned AFTER `pref`,\
+	ADD COLUMN `fame` int(9) unsigned DEFAULT (0) AFTER `pref`,\
 	ADD COLUMN `online` int (2) AFTER `interests`,\
 	ADD COLUMN `ver_code` VARCHAR(100) AFTER `interests`,\
-	ADD COLUMN `verified` int (2) AFTER `ver_code`');
+	ADD COLUMN `verified` int (2) DEFAULT (0) AFTER `ver_code`');
 
     console.log('Sucess!');
     console.log('Exiting...');
