346d96adbd266517030474168ae6ebd012564806
charl KING <cking@c1r2s5.wethinkcode.co.za>
2018-12-14 14:54:36 +0200

    image controllers added to userModels

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -236,7 +236,72 @@ User.prototype.exists = function (callback){
 
 User.prototype.getAllPics = function (callback){
     console.log(this.data);
-	db.query(`SELECT pic FROM pictures WHERE user = '$(this.data.user_name)'`, function (err, results) {
+	db.query(`SELECT pic FROM pictures WHERE user = '${this.data.user_name}'`, function (err, results) {
+		if (err){
+			throw err;
+			callback(err, null);
+		}
+		else{
+			callback(null, results);
+		}
+	})
+}
+
+User.prototype.deletePic = function (callback){
+	console.log(this.data);
+	db.query(`DELETE FROM pictures WHERE id = '${this.data.pic_id}')`, function (err, results) {
+		if (err){
+			throw err;
+			callback(err, null);
+		}
+		else{
+			callback(null, results);
+		}
+	})
+}
+
+User.prototype.savePic = function (callback){
+	console.log(this.data);
+	db.query(`INSERT INTO pictures (pic, user) VALUES '${this.data.pic_data}','${this.data.user_id}')`, function (err, results) {
+		if (err){
+			throw err;
+			callback(err, null);
+		}
+		else{
+			callback(null, results);
+		}
+	})
+}
+
+User.prototype.getProfilePic = function (callback){
+    console.log(this.data);
+	db.query(`SELECT pic FROM pictures WHERE user = '${this.data.user_name}' AND profile_pic = '1'`, function (err, results) {
+		if (err){
+			throw err;
+			callback(err, null);
+		}
+		else{
+			callback(null, results);
+		}
+	})
+}
+
+User.prototype.getPicById = function (callback){
+    console.log(this.data);
+	db.query(`SELECT pic FROM pictures WHERE id = '${this.data.pic_id}'`, function (err, results) {
+		if (err){
+			throw err;
+			callback(err, null);
+		}
+		else{
+			callback(null, results);
+		}
+	})
+}
+
+User.prototype.setProfilePic = function (callback){
+	db.query(`UPDATE TABLE pictures SET profile_pic = '0' WHERE user = '${this.data.user_name}' AND profile_pic = '1'`),
+	db.query(`UPDATE TABLE pictures SET profile_pic = '1' WHERE user = '${this.data.user_name}' AND id = '${this.data.pic_num}'`, function (err, results) {
 		if (err){
 			throw err;
 			callback(err, null);
