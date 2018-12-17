579d95d94b47b1fc676aaa88a478347fdc2f4289
Onex101 <xeno@zarobi.co.za>
2018-12-17 12:44:53 +0200

    Busy with IMage Models

---
+++ server/controllers/imageController.js
@@ -0,0 +1,95 @@
+var User = require('../models/userModel');
+var Image = require('../models/imageModel');
+
+exports.image_create_get = function(req, res){
+	res.send('NOT IMPLEMENTED: image create get');
+}
+
+exports.image_create_post = function(req, res){
+	data = req.body;
+	let image = new Image(data);
+	image.savePic(function (err, result){
+		if (err){
+			res.send('Couldnt not save image');
+			throw err;
+		}
+		else{
+			res.send('Image saved');
+		}
+	})
+	// res.send('NOT IMPLEMENTED: image create post');
+}
+
+exports.image_delete_get = function(req, res){
+	res.send('NOT IMPLEMENTED: image delete get');
+}
+
+exports.image_delete_post = function(req, res){
+	data = req.body;
+	let image = new Image(data);
+	image.deletePic(function(err, results){
+		if (err){
+			res.send({
+						error: 'failed to deleted pic',
+						sucess: null
+					})
+		}
+		else{
+			res.send({
+						error : null,
+						success: 'Succesfully deleted pic'
+					})
+		}
+	})
+	// res.send('NOT IMPLEMENTED: image delete post');
+}
+
+exports.image_update_get = function(req, res){
+	res.send('NOT IMPLEMENTED: image update get');
+}
+
+exports.image_update_post = function(req, res){
+	res.send('NOT IMPLEMENTED: image update post');
+}
+
+exports.images_fetch_get = function(req, res){
+	let id = req.params.id;
+	let image = new Image(data = {id: null, user_id: id, data: null})
+	image.getPicById(function(err, result){
+		if (err){
+			res.send({
+						error: 'failed to get pic',
+						sucess: null
+					})
+		}
+		else{
+			res.json(result)
+		}
+	})
+	res.send('NOT IMPLEMENTED: images fetch get');
+}
+
+exports.images_fetch_post = function(req, res){
+	// res.send('NOT IMPLEMENTED: images fetch post');
+}
+
+exports.image_fetch_get = function(req, res){
+	res.send('NOT IMPLEMENTED: image fetch get');
+}
+
+exports.image_fetch_post = function(req, res){
+	data = req.body;
+	let image = new Image(data)
+	image.getProfilePic(function(err, result){
+		if (err){
+			res.send({
+						error: 'failed to get pic',
+						sucess: null
+					})
+		}
+		else{
+			res.json(result)
+		}
+	})
+	res.send('NOT IMPLEMENTED: image fetch post');
+}
---
+++ server/routes/imageRoutes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var image_controller = require('../controllers/imageController');
+
+/// IMAGES ROUTES ///
+
+// GET request for creating a image.
+router.get('/image/create', image_controller.image_create_get);
+
+// POST request for creating image.
+router.post('/image/create', image_controller.image_create_post);
+
+// GET request to delete image.
+router.get('/image/:id/delete', image_controller.image_delete_get);
+
+// POST request to delete image.
+router.post('/image/:id/delete', image_controller.image_delete_post);
+
+// GET request to update image.
+router.get('/image/:id/update', image_controller.image_update_get);
+
+// POST request to update image.
+router.post('/image/:id/update', image_controller.image_update_post);
+
+// GET request for getting images by id
+router.get('/images/:id', image_controller.images_fetch_get);
+
+// GET request for getting image by id.
+router.get('/image/:id', image_controller.image_fetch_post);
