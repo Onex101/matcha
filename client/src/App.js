840e45e86a33fbf00ef1b4278b8de149a82d26d8
sammyjh30 <sammyjh30@gmail.com>
2019-01-24 11:39:47 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -74,7 +74,8 @@ class App extends Component {
       })
       .then(response => response.json())
       .then((responseJSON) => {
-        // console.log("JSON match test = " + JSON.stringify(responseJSON));
+        console.log("JSON match test = ");
+        console.info(responseJSON);
         this.setState({ userMatches: responseJSON });
         // console.log("APP Matches = " + JSON.stringify(this.state.userMatches));
       })
