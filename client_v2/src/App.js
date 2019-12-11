5ae0a160164d9b3a3c23efb0341a64a3ac761d7f
sammyjh30 <36789037+sammyjh30@users.noreply.github.com>
2019-12-11 07:56:00 +0200

    Setting up client v2

--- client_v2/src/App.js
+++ client_v2/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Routes from "./Routes";
 
 function App() {
+  const childProps = {
+    // isAuthenticated       : this.state.isAuthenticated,
+    // userHasAuthenticated  : this.userHasAuthenticated,
+    // userInfo              : this.state.userInfo,
+    // userProfile           : this.state.userProfile,
+    // userMatches           : this.state.userMatches,
+    // setUser               : this.setUser,
+    // socket                : this.state.socket,
+    // socketUser            : this.state.socketUser,
+    // getSearchResults      : this.getSearchResults,
+    // gotResults            : this.gotResults,
+    // results               : this.state.results,
+    // resultType            : this.state.resultType,
+    // getMatches            : this.getMatches
+  };
+  // var style = {
+  //   display: 'flex',
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,8 +39,50 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="App-content">
+          <Routes childProps={childProps} />
+        </div>
     </div>
+    
   );
 }
 
 export default App;
+
+
+//  <div className="App-setup">
+//       <div className="App-container">
+//         <Navbar fluid collapseOnSelect>
+//           <Navbar.Header>
+//             <Navbar.Brand>
+//               <Link to="/" className="App-logo">Matcha</Link>
+//             </Navbar.Brand>
+//             <div style={style}>
+//               {this.searchCheck()}
+//               <Navbar.Toggle />
+//             </div>
+//           </Navbar.Header>
+//           {localStorage.getItem('user')
+//             ? this.renderUser()
+//             : <Navbar.Collapse><Nav pullRight>
+//               <Fragment>
+//                 <LinkContainer to="/signup">
+//                   <NavItem>Signup</NavItem>
+//                 </LinkContainer>
+//                 <LinkContainer to="/login">
+//                   <NavItem>Login</NavItem>
+//                 </LinkContainer>
+//               </Fragment>
+//             </Nav></Navbar.Collapse>
+//           }
+//         </Navbar>
+//         {console.log("Childprops: ")}
+//         {console.log("Test for re-render")}
+//         {console.info(childProps)}
+//         {this.state.redirect ?
+//           <Redirect push to="/searchResults" /> : null}
+//         <div className="App-content">
+//           <Routes childProps={childProps} />
+//         </div>
+//       </div>
+//     </div> 
