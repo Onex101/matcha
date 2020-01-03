c59059052d02cff95f6173ac5837a2941b623de2
Onex101 <xeno@zarobi.co.za>
2020-01-03 13:41:00 +0200

    Update getUsersThatLikeCurrentUSer

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -87,7 +87,7 @@ User.prototype.linked_users = function(id, callback){
 }
 
 User.prototype.getUsersThatLikeCurrentUser = function(id, callback){
-	var query = `SELECT users.*, pictures.pic FROM users INNER JOIN (SELECT user1_id AS id FROM likes WHERE user2_id = ${id} AND link_code = 1) AS lst ON lst.id = users.id INNER JOIN pictures ON users.profile_pic_id = pictures.id`;
+	var query = `SELECT users.*, pictures.pic FROM users JOIN (SELECT user1_id AS id FROM likes WHERE user2_id = ${id} AND link_code = 1) AS lst ON lst.id = users.id INNER JOIN pictures ON users.profile_pic_id = pictures.id`;
 	db.query(query, function(err,result){
 		if (err) {callback(err, null);}
         else{
