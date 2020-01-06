5537c7b30367eebbaa8e8be6ee9401b56e044796
Xeno RHODA <xeno@zarobi.co.za>
2020-01-06 08:28:44 +0200

    Remove Checks

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -167,11 +167,11 @@ exports.user_update_get = function(req, res) {
 // Handle User update on POST.
 exports.user_update_post = function(req, res) {
 	let user = new User(req.body);
-	if (!user.data.bio || !user.data.gender || !user.data.pref || !user.data.id || !user.data.first_name || !user.data.last_name || !user.data.email){
-		console.log(user)
-		res.send({error: "Not all user values are present", user: user});
-		return;
-	}
+	// if (!user.data.bio || !user.data.gender || !user.data.pref || !user.data.id || !user.data.first_name || !user.data.last_name || !user.data.email){
+	// 	console.log(user)
+	// 	res.send({error: "Not all user values are present", user: user});
+	// 	return;
+	// }
 	user.update_data(user.data.bio, user.data.gender, user.data.pref, user.data.id, user.data.first_name, user.data.last_name, user.data.email, function (err, results){
 		if(err){res.send(err)}
 		else{
